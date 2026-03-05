2b2bce64aa38f1f4b0cdbba943151a09918198a2
Adam Williams <adamwilliams0401@gmail.com>
2026-03-05 21:06:43 +0000

    Added prompt loading

---
+++ papery/core/prompt/prompt.py
@@ -0,0 +1,17 @@
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
+from typing import Any
+from papery.core.utils import get_project_root
+
+prompts_dir = get_project_root() / "data" / "prompts"
+
+def get_prompt(name: str, **settings: dict[str, Any]) -> str:
+    env = Environment(
+        loader=FileSystemLoader(prompts_dir),
+        undefined=StrictUndefined)
+    template = env.get_template(name)
+
+    rendered_prompt = template.render(
+        **settings
+    )
+
+    return rendered_prompt
