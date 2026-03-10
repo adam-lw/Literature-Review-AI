3361e2cb97121e3d50585b1e9bfb0598fea853e7
Adam Williams <adamwilliams0401@gmail.com>
2026-03-10 23:53:50 +0000

    Added specter embedding collection by ID

--- src/papery/core/api/semantic_scholar.py
+++ src/papery/core/api/semantic_scholar.py
@@ -1,5 +1,5 @@
 """
-Pipelines for building a vector database
+API wrapper for the Semantic Scholar API, including helper methods for bulk paper collection and embedding collection.
 
 
 """
@@ -7,10 +7,11 @@ Pipelines for building a vector database
 from papery.core.api.core import api_call
 import pandas as pd
 import asyncio
-from typing import Optional, Any, Literal
+from typing import Optional, Literal
 
 from papery.core.utils import load_dict, get_project_root
 from papery.core.db import load_table, save_table_async, get_inspector
+from papery.core.db import ENGINE as db_engine
 
 from loguru import logger
 
@@ -19,7 +20,6 @@ CONFIG_PATH = get_project_root() / "config/api/semantic_scholar.json"
 
 async def bulk_collect_papers(
     query: str,
-    table_save_path: str,
     research_fields: list[str],
     *,
     sort_by: Optional[str] = None,
@@ -38,14 +38,16 @@ async def bulk_collect_papers(
         "MetaAnalysis",
     ],
     years: Optional[tuple[int, int]] = None,
-    n_results: int = 1000,
     open_access_only: bool = True,
     min_citation_count: int = 3,
-) -> dict[str, Any]:
+    verbosity: int = 0,
+):
     """
     Helper method for calling the Semantic Scholar paper API for bulk paper collection.
     """
-    logger.info(f"Collecting papers for query `{query}`")
+    if verbosity > 0:
+        logger.info(f"Collecting papers for query `{query}`")
+
     config = load_dict(CONFIG_PATH)
 
     # Validate data
@@ -69,12 +71,6 @@ async def bulk_collect_papers(
     if sort_by and sort_by not in config["return_fields"]:
         raise ValueError(f"Unrecognised sort_by field {sort_by}")
 
-    inspector = get_inspector()
-    if not inspector.has_schema(str(table_save_path).split(".")[0]):
-        raise ValueError(
-            f"Schema {table_save_path.split('.')[0]} does not exist. Please create the schema before running the pipeline."
-        )
-
     # Prepare data
     if sort_by is not None and ascending is not None:
         sort_by_str = sort_by + (":asc" if ascending else ":desc")
@@ -89,20 +85,16 @@ async def bulk_collect_papers(
     fields_of_study_str = ",".join(research_fields)
     years_str = "-".join([str(y) for y in years]) if years is not None else None
 
-    metadata = {"query": query}
     continuation_token: str = ""
-    remaining_papers = n_results
-    while remaining_papers > 0:
-        print(f"Remaining papers: {remaining_papers}")
-        n_papers_to_collect = remaining_papers if remaining_papers < 1000 else 1000
-        remaining_papers -= n_papers_to_collect
-
+    total_collected = 0
+    limit = 0
+    while True:
         params = {
             "query": query,
             "token": continuation_token if len(continuation_token) > 0 else None,
             "fields": return_fields_str,
             "sort": sort_by_str,
-            "limit": n_results,
+            "limit": 5000000,
             "publicationTypes": publication_types_str,
             "fieldsOfStudy": fields_of_study_str,
             "year": years_str,
@@ -125,80 +117,86 @@ async def bulk_collect_papers(
         if data.get("error", None):
             logger.error(f"Error in API response: {data['error']}")
             logger.error(f"Full response: {data}")
-            metadata["error"] = data["error"]
             break
 
-        continuation_token = data.get("token", "")
+        if limit == 0:
+            limit = data.get("total", 0)
 
-        if "n_found" not in metadata.keys():
-            metadata["n_found"] = data.get("total", 0)
+        if limit == 0:
+            logger.error(f"No matches returned for query: `{query}`.")
+            break
+
+        if total_collected >= limit:
+            logger.info(f"Exhausted available papers for {query}")
+
+        continuation_token = data.get("token", "")
 
         results = pd.DataFrame(data.get("data", None))
 
         if len(results) == 0:
             logger.error(f"No data returned. Full response: {data}")
 
-        if data.get("total", 0) == 0:
-            logger.error(
-                f"No matches returned. {query} Remaining results {remaining_papers} Full response: {data}"
-            )
-
         # The API will sometimes return fields we didn't ask for - drop them
         results = results.drop(
             columns=[c for c in results.columns if c not in return_fields]
         )
 
-        await save_table_async(results, table_path=table_save_path, if_exists="append")
-
-        if n_results > data.get("total", 0):
-            logger.warning(
-                "Warning: total matches is lower than desired matches. Stopping"
-            )
-            break
-
-    return metadata
+        yield results
 
 
-async def collect_embeddings(
-    table_uri: str, id_col: str, embedding: Literal["specter_v1", "specter_v2"]
+async def collect_specter_embeddings(
+    ids: list[str], embedding: Literal["specter_v1", "specter_v2"]
 ):
-    data = load_table(table_uri)
+    """
+    Generator method for collecting SPECTER paper embeddings in bulk, given a list of paper IDs.
 
-    ids = data[id_col].tolist()
-
+    This is implemented seperately from the standard EmbeddingModel structure as this relies on
+    retrieval of embeddings by ID, rather than embedding generation.
+    """
     id_chunks = [ids[i : i + 500] for i in range(0, len(ids), 500)]
 
-    response = await asyncio.gather(
-        *[
+    tasks = [
+        asyncio.create_task(
             api_call(
                 header={"fields": f"paperId,embedding.{embedding}"},
                 body={"ids": chunk},
                 api_id="semantic_scholar_api",
                 endpoint="graph/v1/paper/batch",
                 task="POST",
+                verbosity=1,
             )
-            for chunk in id_chunks
-        ][0:2]
-    )
-
-    print(response[0].keys())
-
-
-# if __name__ == "__main__":
-
-#     load_dotenv()
-
-#     papers = asyncio.run(bulk_collect_papers(
-#         query="machine learning",
-#         table_save_path="test.test_papers",
-#         research_fields=["Medicine", "Computer Science"],
-#         publication_types=["JournalArticle", "Conference"],
-#         n_results=2500)
-#     )
-
-if __name__ == "__main__":
-    asyncio.run(
-        collect_embeddings(
-            table_uri="test.test_papers", id_col="paperId", embedding="specter_v2"
         )
-    )
+        for chunk in id_chunks
+    ]
+
+    for task in asyncio.as_completed(tasks):
+        response = await task
+        if response.get("code", None) != 200:
+            logger.error("Error in API response.")
+            logger.error(f"Full response: {response}")
+            break
+
+        resp_data = response.get("data", None)
+        if resp_data is None:
+            logger.error(f"No data field in response. Full response: {response}")
+            break
+
+        # Format data
+        formatted_data = []
+        for paper in resp_data:
+            if paper is None:
+                logger.warning("Null value encountered in response")
+                continue
+            elif paper.get("embedding", None) is None:
+                print(f"No embedding for paperId {paper['paperId']}")
+                continue
+            formatted_data.append(
+                {
+                    "paperId": paper["paperId"],
+                    embedding: paper.get("embedding", {}).get("vector", None),
+                }
+            )
+
+        yield pd.DataFrame(formatted_data)
+
+    return
